7fecf1b4f9dd1278e4f525b19eefcf0dd881a803
danielcsulb <danielmendia86@gmail.com>
2017-11-03 00:09:36 -0700

    messages

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const message = prompt('Enter a message')
+const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit')
 
 // alert('Your input is: ' + message)
-const output = document.getElementById("output")
-output.innerHTML = 'Your message is ' + message
+// const output = document.getElementById("output")
+// output.innerHTML = 'Your message is ' + message
+
+const messages = [
+    'message 01',
+    'message 02',
+    'message 03',
+    'message 04',
+    'message 05',
+]
+
+function list() {
+
+
+    for (var i = 0; i < messages.length; i++) {
+        document.write((i + 1) + ":" + messages[i] + "<br/>")
+    }
+
+}
+
+if (message == 1) {
+    const output = document.getElementById("output")
+    output.innerHTML = "The current messages are: </br>" + list().innerHTML;
+}
