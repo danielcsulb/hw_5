562fe83f5fe83b3d111c2dbf652eb3c1c3d2ee84
danielcsulb <danielmendia86@gmail.com>
2017-11-05 11:59:17 -0800

    comit

--- app.js
+++ app.js
@@ -2,95 +2,107 @@
 
 // while (userIn != 0) {
 var userchoice = true;
+var userIn = "";
+// while (userchoice = true) {
+// while (userIn == "0" || "" || "1" || "2" || "3") {
 
-do {
-    const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
-    var userIn = message;
+const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
 
+userIn = parseInt(message, 10);
 
-    // while (message != 0) {
-    // alert('Your input is: ' + message)
-    // const output = document.getElementById("output")
-    // output.innerHTML = 'Your message is ' + message
-    // while (message != 0) {
-    const messages = [
-        'message 01',
-        'message 02',
-        'message 03',
-        'message 04',
-        'message 05',
-    ]
+// do {
+// while (userIn == 0 || "" || 1 || 2 || 3) {
+// const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+// var userIn = message;
 
 
-    function list() {
-        for (var i = 0; i < messages.length; i++) {
-            document.write((i + 1) + ": " + messages[i] + "<br/>")
-        }
+// while (message != 0) {
+// alert('Your input is: ' + message)
+// const output = document.getElementById("output")
+// output.innerHTML = 'Your message is ' + message
+// while (message != 0) {
+const messages = [
+    'message 01',
+    'message 02',
+    'message 03',
+    'message 04',
+    'message 05',
+];
 
+
+function list() {
+    for (var i = 0; i < messages.length; i++) {
+        document.write((i + 1) + ": " + messages[i] + "<br/>")
     }
 
-    // // while (userIn > 0 && userIn <= 3) {
-    // //     // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+}
+
+// // while (userIn > 0 && userIn <= 3) {
+// //     // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
 
 
-    if (userIn == 1) {
-        const output = document.getElementById("output")
-        output.innerHTML = 'The current messages are: ';
-        output2.innerHTML = list().innerHTML;
-        userchoice = true;
-        // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
-    }
-
+if (userIn == "1") {
+    const output = document.getElementById("output");
+    output.innerHTML = 'The current messages are: ';
+    output2.innerHTML = list().innerHTML;
+    // userchoice = true;
     // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+    // continue;
+}
 
-    if (userIn == 2) {
-        const message2 = prompt("Enter a new message: ");
+// const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
 
-        messages.push(message2);
-        const output = document.getElementById("output")
-        output.innerHTML = 'The current messages are: ';
-        output2.innerHTML = list().innerHTML;
-        userchoice = true;
-        // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+if (userIn == "2") {
+    const message2 = prompt("Enter a new message: ");
 
-    }
+    messages.push(message2);
+    const output = document.getElementById("output");
+    output.innerHTML = 'The current messages are: ';
+    output2.innerHTML = list().innerHTML;
+    // userchoice = true;
+    // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+    // continue;
 
-    if (userIn == 3) {
-        const message3 = prompt("Enter the message index between 1 and " + messages.length);
+}
 
-        var m = parseInt(message3) - 1;
+if (userIn == "3") {
+    const message3 = prompt("Enter the message index between 1 and " + messages.length);
+    var m = parseInt(message3) - 1;
 
-        messages.splice(m, 1)
-        const output = document.getElementById("output")
-        output.innerHTML = 'The current messages are: ';
-        output2.innerHTML = list().innerHTML;
-        userchoice = true;
-        // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+    messages.splice(m, 1);
+    const output = document.getElementById("output");
+    output.innerHTML = 'The current messages are: ';
+    output2.innerHTML = list().innerHTML;
+    // userchoice = true;
+    // const message = prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete message \n 0: Quit');
+    // continue;
 
-    }
+}
+// else {
+if (userIn == "0") {
+    const output = document.getElementById("output");
+    output.innerHTML = 'Good Bye';
+    // userchoice = false;
+    // break;
+}
 
-    if (userIn == 0) {
-        const output = document.getElementById("output")
-        output.innerHTML = 'Good Bye';
-        userchoice = false;
-    }
+if (userIn === "") {
+    const output = document.getElementById("output");
+    output.innerHTML = 'Please refresh and select correct command.';
+    // userchoice = false;
+    // break;
+}
 
-    if (userIn === "") {
-        const output = document.getElementById("output")
-        output.innerHTML = 'Please refresh and select correct command.'
-        userchoice = false;
-    }
+if (isNaN(userIn)) {
+    const output = document.getElementById("output");
+    output.innerHTML = 'Please refresh and select correct command.';
+    // userchoice = false;
+    // break;
+}
 
-    if (isNaN(userIn)) {
-        const output = document.getElementById("output")
-        output.innerHTML = 'Please refresh and select correct command.'
-        userchoice = false;
-    }
-
-} while (userchoice == true);
+// } while (userchoice == true);
 // while (userchoice > 0 && userchoice <= 3);
-
-
+// }
 
 
 // }
